24d0bb62f5292454eee99378c3fe52f5a1bf97d4
Jacashi55504 <jacashi@hotmail.com>
2024-12-05 14:59:04 -0600

    t

--- src/test/java/com/mayab/quality/functional/SeleniumTest.java
+++ src/test/java/com/mayab/quality/functional/SeleniumTest.java
@@ -1,114 +1,77 @@
-package com.mayab.quality.functional;
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
-import org.junit.*;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-import org.apache.commons.io.FileUtils;
-import java.io.File;
+import org.junit.*;
 import java.time.Duration;
+
 public class SeleniumTest {
-	private WebDriver driver;
-	  private String baseUrl;
-	  private boolean acceptNextAlert = true;
-	  private StringBuffer verificationErrors = new StringBuffer();
-	  JavascriptExecutor js;
-	  
-	  @Before
-	  public void setUp() throws Exception {
-		  WebDriverManager.chromedriver().setup();
-		  ChromeOptions options = new ChromeOptions();
-		  options.addArguments("--headless"); // Ejecutar en modo headless
-		  options.addArguments("--disable-gpu"); // Deshabilitar GPU (opcional, mejora en headless)
-		  options.addArguments("--no-sandbox"); // Recomendado para contenedores
-		  options.addArguments("--disable-dev-shm-usage"); // Usar /tmp si falta memoria compartida
-		  driver = new ChromeDriver(options);
-		  baseUrl = "https://www.facebook.com/";
-		  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
-		  js = (JavascriptExecutor) driver;
-	  }
-	  
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+    JavascriptExecutor js;
 
-	  @Test
-	  public void testUntitledTestCase() throws Exception {
-	      driver.get("https://www.facebook.com/");
-	      pause(5000);
-	      
-	      driver.findElement(By.id("email")).clear();
-	      driver.findElement(By.id("email")).sendKeys("fakemail@gmail.com");
-	      
-	      driver.findElement(By.id("pass")).clear();
-	      driver.findElement(By.id("pass")).sendKeys("password");
+    @Before
+    public void setUp() throws Exception {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // Ejecutar en modo headless
+        options.addArguments("--disable-gpu"); // Deshabilitar GPU
+        options.addArguments("--no-sandbox"); // Recomendado para contenedores
+        options.addArguments("--disable-dev-shm-usage"); // Evitar problemas de memoria compartida
+        options.addArguments("--window-size=1920,1080"); // Definir tamaño de ventana
+        driver = new ChromeDriver(options);
+        baseUrl = "https://www.facebook.com/";
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+        js = (JavascriptExecutor) driver;
+    }
 
-	      driver.findElement(By.name("login")).click();
-	      
-	      WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-	      // Agarrar el mensaje capturado en el elemento 
-	      WebElement errorElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(@class, '_9ay7')]")));
+    @Test
+    public void testUntitledTestCase() throws Exception {
+        driver.get(baseUrl);
+        pause(5000);
 
-	      // Handling para error
-	      String actualResult = errorElement.getText();
-	      System.out.println("Mensaje de error encontrado: " + actualResult);
+        // Verificar si el campo de email está presente antes de interactuar
+        WebElement emailField = new WebDriverWait(driver, Duration.ofSeconds(10))
+            .until(ExpectedConditions.presenceOfElementLocated(By.id("email")));
+        emailField.clear();
+        emailField.sendKeys("fakemail@gmail.com");
 
-	      assertThat(actualResult, is("The password you’ve entered is incorrect.\nForgot Password?"));
-	  }
+        WebElement passwordField = driver.findElement(By.id("pass"));
+        passwordField.clear();
+        passwordField.sendKeys("password");
 
-	  @After
-	  public void tearDown() throws Exception {
-	    driver.quit();
-	    String verificationErrorString = verificationErrors.toString();
-	    if (!"".equals(verificationErrorString)) {
-	      fail(verificationErrorString);
-	    }
-	  }
-	  
-	  private void pause(long mils) {
-	      try {
-	          Thread.sleep(mils);
-	      } catch (InterruptedException e) {
-	          e.printStackTrace();
-	      }
-	  }
+        driver.findElement(By.name("login")).click();
 
-	  private boolean isElementPresent(By by) {
-	    try {
-	      driver.findElement(by);
-	      return true;
-	    } catch (NoSuchElementException e) {
-	      return false;
-	    }
-	  }
+        // Esperar y validar el mensaje de error
+        WebElement errorElement = new WebDriverWait(driver, Duration.ofSeconds(15))
+            .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(@class, '_9ay7')]")));
 
-	  private boolean isAlertPresent() {
-	    try {
-	      driver.switchTo().alert();
-	      return true;
-	    } catch (NoAlertPresentException e) {
-	      return false;
-	    }
-	  }
+        String actualResult = errorElement.getText();
+        System.out.println("Mensaje de error encontrado: " + actualResult);
 
-	  private String closeAlertAndGetItsText() {
-	    try {
-	      Alert alert = driver.switchTo().alert();
-	      String alertText = alert.getText();
-	      if (acceptNextAlert) {
-	        alert.accept();
-	      } else {
-	        alert.dismiss();
-	      }
-	      return alertText;
-	    } finally {
-	      acceptNextAlert = true;
-	    }
-	  }
+        Assert.assertEquals("The password you’ve entered is incorrect.\nForgot Password?", actualResult);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            Assert.fail(verificationErrorString);
+        }
+    }
+
+    private void pause(long mils) {
+        try {
+            Thread.sleep(mils);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
