df7d1795cf5e2849b93585ddec4842ef3d0d69e4
Jacashi55504 <jacashi@hotmail.com>
2024-12-05 11:41:28 -0600

    S

--- src/test/java/com/mayab/quality/integration/UserDAOTest.java
+++ src/test/java/com/mayab/quality/integration/UserDAOTest.java
@@ -1,85 +1,124 @@
-version: 2.1
+package com.mayab.quality.integration;
 
-executors:
-  java-executor:
-    docker:
-      - image: circleci/openjdk:11  # Java 11
-      - image: mysql:8.0  # Contenedor MySQL para pruebas de integración
-        environment:
-          MYSQL_ROOT_PASSWORD: root
-          MYSQL_DATABASE: calidad2024
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-jobs:
-  unit_tests:
-    executor: java-executor
-    steps:
-      - checkout
+import java.io.File;
+import java.io.FileInputStream;
 
-      # Instalar Maven (si no está preinstalado)
-      - run:
-          name: Install Maven
-          command: sudo apt-get update && sudo apt-get install -y maven
+import org.dbunit.Assertion;
+import org.dbunit.DBTestCase;
+import org.dbunit.PropertiesBasedJdbcDatabaseTester;
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.database.QueryDataSet;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-      # Compilar y ejecutar pruebas unitarias
-      - run:
-          name: Run Unit Tests
-          command: mvn clean test -Dtest=!UserDAOTest
+import com.mayab.quality.logginunittest.dao.UserMysqlDAO;
+import com.mayab.quality.logginunittest.model.User;
 
-  integration_tests:
-    executor: java-executor
-    steps:
-      - checkout
+class UserDAOTest extends DBTestCase{
+	
+	UserMysqlDAO daoMysql;
+	
+	public UserDAOTest() {
+		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "com.mysql.cj.jdbc.Driver");
+		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, "jdbc:mysql://localhost:3307/calidad2024");
+		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, "root");
+		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, "123456");
+	}
 
-      # Asegurarse de que MySQL esté listo antes de las pruebas
-      - run:
-          name: Wait for MySQL
-          command: |
-            for i in {1..30}; do
-              nc -z localhost 3306 && echo "MySQL is up!" && break
-              echo "Waiting for MySQL..."
-              sleep 1
-            done
+	@BeforeEach
+	protected void setUp() throws Exception {
+		// Initialize DAO
+		
+		daoMysql = new UserMysqlDAO(); 
+		
+		// Set the initial condition of the database
+		
+		IDatabaseConnection connection = getConnection(); 
+		connection.getConfig().setProperty(DatabaseConfig.FEATURE_CASE_SENSITIVE_TABLE_NAMES, false);
+		try {
+			DatabaseOperation.TRUNCATE_TABLE.execute(connection,getDataSet());
+			DatabaseOperation.CLEAN_INSERT.execute(connection, getDataSet());						
+		} catch(Exception e) {
+			fail("Error in setup: "+ e.getMessage()); 
+		} finally {
+			connection.close(); 
+		}
+	}
 
-      # Compilar el proyecto y ejecutar pruebas de integración
-      - run:
-          name: Run Integration Tests
-          command: mvn clean test -Dtest=UserDAOTest
+	 protected IDataSet getDataSet() throws Exception {
+	        return new FlatXmlDataSetBuilder().build(new FileInputStream("src/resources/initDB.xml"));
+	 }
+	 
+	@Test
+	public void testAddUser() {
+		//Init
+		User user = new User("Diego Barba", "diego.barba@example.com", "newPassword");
+		
+		//Exercise
+		daoMysql.save(user);
+			
+		// Verify data in database
+		try {
+			// This is the full database
+			IDatabaseConnection conn = getConnection();
+			conn.getConfig().setProperty(DatabaseConfig.FEATURE_CASE_SENSITIVE_TABLE_NAMES, false);
+			// org.dbunit.database.AmbiguousTableNameException: USUARIOS
+			IDataSet databaseDataSet = conn.createDataSet(); 
+			String[] tables = databaseDataSet.getTableNames();
+				
+			ITable actualTable = databaseDataSet.getTable("usuarios");
+				
+			// Read XML with the expected result
+			IDataSet expectedDataSet = new FlatXmlDataSetBuilder().build(new File("src/resources/createUser.xml"));
+			ITable expectedTable = expectedDataSet.getTable("usuarios");
+				
+			Assertion.assertEquals(expectedTable, actualTable);
+				
+				
+		} catch (Exception e) {
+			// TODO: handle exception
+			fail("Error in insert test: " + e.getMessage());
+		}	
+	}
+		
+	@Test
+	public void testAddUserQry() {
+		//Init
+		User user = new User("username2", "correo2@correo.com", "123456");
+		//Exercise
+		int newID = daoMysql.save(user);
+				
+		// Verify data in database
+		try {
+			// This is the full database
+			IDatabaseConnection conn = getConnection();
+			IDataSet databaseDataSet = conn.createDataSet(); 
+			
+			QueryDataSet actualTable = new QueryDataSet(getConnection());
+						
+			actualTable.addTable("inserTMP", "SELECT * FROM usuarios WHERE id = " + newID);
+						
+			String actualName = (String) actualTable.getTable("inserTMP").getValue(0, "name");
+			String actualEmail = (String) actualTable.getTable("inserTMP").getValue(0, "email");
+			String actualPassword = (String) actualTable.getTable("inserTMP").getValue(0, "password");
+						
+			assertThat(actualName, is(user.getName()));
+			assertThat(actualEmail, is(user.getEmail()));
+			assertThat(actualPassword, is(user.getPassword()));
+						
+		} catch (Exception e) {
+			// TODO: handle exception
+			fail("Error in insert test: " + e.getMessage());
+		}	
+		
+	}
 
-  functional_tests:
-    executor: java-executor
-    steps:
-      - checkout
-
-      # Instalar Google Chrome
-      - run:
-          name: Install Google Chrome
-          command: |
-            wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-            echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
-            sudo apt-get update
-            sudo apt-get install -y google-chrome-stable
-
-      # Descargar y configurar ChromeDriver
-      - run:
-          name: Install ChromeDriver
-          command: |
-            CHROME_DRIVER_VERSION=$(wget -qO- https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
-            wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
-            unzip chromedriver_linux64.zip
-            sudo mv chromedriver /usr/local/bin/
-            sudo chmod +x /usr/local/bin/chromedriver
-
-      # Ejecutar pruebas funcionales con Selenium
-      - run:
-          name: Run Functional Tests
-          command: mvn clean test -Dtest=CRUDSeleniumTest
-
-workflows:
-  version: 2
-  build_and_test:
-    jobs:
-      - unit_tests
-      - integration_tests:
-          requires:
-            - unit_tests
-      - functional_tests
+}
